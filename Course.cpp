8b7853c53fc812ed179c20d80f1d92bf44c1c85b
MajesticMagnus48 <mitchomackay@hotmail.com>
2022-11-21 22:10:01 -0500

    made test assignment method

--- Course.cpp
+++ Course.cpp
@@ -15,6 +15,20 @@ private:
 public:
 
     void setTest(int testNum, float grade) {
+        if(testNum == 1){
+            testGrade1 = grade;
+        }
+        else if (testNum == 2)
+        {
+            testGrade2 = grade;
+        }
+        else if (testNum == 3)
+        {
+            testGrade3 = grade;
+        }
+        else{
+            std::cout << "Not a valid test number!";
+        }
 
     }
 
