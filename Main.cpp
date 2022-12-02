d98a813b166564793a92252be2bf2237751cb7fb
MajesticMagnus48 <mitchomackay@hotmail.com>
2022-12-01 21:47:10 -0500

    Added plenty of offenssive programming checks

--- Main.cpp
+++ Main.cpp
@@ -14,10 +14,13 @@ using namespace std;
 int main() {
 	//Initialize variables
 	string line, file1, file2;
+
+	//Get user input for text files
 	cout << "Enter the first filename: ";
 	cin >> file1;
 	cout << "Enter the second filename: ";
 	cin >> file2;
+
 	ifstream myfile (file1);
 	Student *students = new Student[100];
 	int count = 0;
@@ -35,13 +38,28 @@ int main() {
 				getline(s_stream, substr, ',');
 				result.push_back(substr);
 			}
-			//Resulting vector array is [0] = studentid, [1] = student name
+			//Resulting vector is [0] = studentid, [1] = student name
 			//Assign substrings appropriately
+			//Check that commas are valid
 			if(result.size() == 2) {
-				students[count].setId(stoi(result[0]));
+				//Ensure that student Id is correct length
+				if(result[0].length() <= 9) {
+					students[count].setId(stoi(result[0]));
+				}
+				else {
+					cout << "Invalid student Ids (File 2)";
+					exit(4);
+				}
 
-				result[1] = result[1].substr(1, result[1].length());
-				students[count].setName(result[1]);
+				//Ensure that student name is correct length
+				if(result[1].substr(1, result[1].length()).length() <= 20) {
+					result[1] = result[1].substr(1, result[1].length());
+					students[count].setName(result[1]);
+				}
+				else {
+					cout << "Invalid student Names (File 1)";
+					exit(4);
+				}
 
 				//Increment count to target next student
 				count = count + 1;
@@ -74,14 +92,27 @@ int main() {
 				getline(s_stream, substr, ',');
 				result.push_back(substr);
 			}
+
+			//Check if student ids are valid
+			if(result[0].length() > 9) {
+				cout << "Invalid student ids (file 2)";
+				exit(4);
+			}
+
 			//Determine the count values of the correctly matched student ids
-			while(stoi(result[0]) != students[count].getId()) {
+			while(stoi(result[0]) != students[count].getId() && students[count].getId() != 0) {
 				count = count + 1;
 			}
-			//Resulting vector array is [0] = studentid, [1] = student name, [2] = test1, [3] = test2, [4] = test3, [5] = final
+			//Resulting vector is [0] = studentid, [1] = student name, [2] = test1, [3] = test2, [4] = test3, [5] = final
 			//Assign matched student with correct course data
 
+			//Checks if commas are valid
 			if(result.size() == 6) {
+				//Check if course codes are valid
+				if(result[1].length() > 6) {
+					cout << "Invalid Course codes";
+					exit(4);
+				}
 				students[count].courseData.insert(Course(result[1].substr(1, result[1].length()), stof(result[2].substr(1, result[2].length())), stof(result[3].substr(1, result[3].length())),
 				stof(result[4].substr(1, result[4].length())), stof(result[5].substr(1, result[5].length()))));
 			}
